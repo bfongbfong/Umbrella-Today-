7c3728ef76ece9abdce0afb635c6db6ee2d945d4
Brandon Fong <bfongbfong@gmail.com>
2019-12-29 14:59:43 -0500

    Moves autocomplete search methods into singleton

--- Umbrella Today?/Controller/AutocompleteSearchManager.swift
+++ Umbrella Today?/Controller/AutocompleteSearchManager.swift
@@ -40,4 +40,59 @@ class AutocompleteSearchManager {
             }
         }
     }
+    
+    static func getAutocompleteCities(jsonArray: [Any], range: Range, input: [Character]) -> [City] {
+        // it is a modified binary search
+        
+        var cities = [City]()
+        // goal is to use binary search to search within the range for first appearance of city name matching what the user has typed
+        // ex: man == manhattan, manhasset, mandalorian, etc.
+        // we want to display at max 10 results.
+        
+        // maybe what we can do is do binary search until we find something that satisfies
+        // then move left until we find what doesn't. using what we have cached (the places we've already looked on the left)
+        // then, if we haven't hit our max of 10, we can also move rightwards too
+        
+        guard let firstMatchIndex = findFirstMatchIndex(jsonArray: jsonArray, range: range, input: input) else {
+            return []
+        }
+        
+        guard let cityObject = jsonArray[firstMatchIndex] as? [String: Any] else {
+            // i think return empty if the middle element doesn't exist, or isn't a dictionary?
+            return []
+        }
+        guard let cityName = cityObject["name"] as? String else {
+            return []
+        }
+        
+        guard let countryName = cityObject["country"] as? String else {
+            return []
+        }
+        
+        let firstMatchedCity = City(name: cityName, country: countryName)
+        cities.append(firstMatchedCity)
+        
+        for i in 1...9 {
+            guard let cityObject = jsonArray[firstMatchIndex + i] as? [String: Any] else {
+                // i think return empty if the middle element doesn't exist, or isn't a dictionary?
+                return cities
+            }
+            guard let cityName = cityObject["name"] as? String else {
+                return cities
+            }
+            
+            let cityNameChars = Array(cityName)
+            
+            if Array(cityNameChars[0...input.count - 1]) == input {
+                guard let countryName = cityObject["country"] as? String else {
+                    return cities
+                }
+                
+                let thisCity = City(name: cityName, country: countryName)
+                cities.append(thisCity)
+            }
+        }
+        
+        return cities
+    }
 }
