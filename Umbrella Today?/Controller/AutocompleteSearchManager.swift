11aae4a8c7791dda096c559d58fb86c5c02b40b8
Brandon Fong <bfongbfong@gmail.com>
2019-12-29 14:58:57 -0500

    Moves autocomplete search methods into singleton

--- Umbrella Today?/Controller/AutocompleteSearchManager.swift
+++ Umbrella Today?/Controller/AutocompleteSearchManager.swift
@@ -9,5 +9,35 @@
 import Foundation
 
 class AutocompleteSearchManager {
-    
+    static func searchForCityID(cityName: String) {
+        let capitalizedCityName = cityName.capitalized
+        let cityNameCharacters = Array(capitalizedCityName)
+
+        if let path = Bundle.main.url(forResource: "city.list.sorted", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: path)
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options: [])
+                
+                guard let jsonArray = jsonResult as? [Any] else {
+                    print("Data of file could not be made casted into an array")
+                    return
+                }
+                
+                var range = Range(leftIndex: 0, rightIndex: jsonArray.count)
+
+                findLetterCheckPointAndSetRange(cityNameCharacters: cityNameCharacters, range: &range)
+                print("range ---- leftIndex:", range.leftIndex)
+                print("range ---- rightIndex:", range.rightIndex)
+                
+                let cities = getAutocompleteCities(jsonArray: jsonArray, range: range, input: cityNameCharacters)
+                cities.forEach { (city) in
+                    print(city.name)
+                }
+                
+            } catch {
+               // handle error
+                print("caught error")
+            }
+        }
+    }
 }
