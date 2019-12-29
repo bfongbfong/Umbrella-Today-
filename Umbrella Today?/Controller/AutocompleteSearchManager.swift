fa7bc854a74995965166e05ff36bb74d144281b6
Brandon Fong <bfongbfong@gmail.com>
2019-12-29 16:54:09 -0500

    Includes docuemntation comments in AutocompleteSearchManager

--- Umbrella Today?/Controller/AutocompleteSearchManager.swift
+++ Umbrella Today?/Controller/AutocompleteSearchManager.swift
@@ -10,8 +10,18 @@ import Foundation
 
 class AutocompleteSearchManager {
     
+    /// Accesses JSON file of city objects to complete city autocomplete search.
     ///
-    static func searchForCities(cityName: String, maxNumberOfResults: Int) {
+    /// - Parameters:
+    ///     - cityName: String of input for autocomplete search
+    ///     - maxNumberOfResults: The maximum number of results desired to be returned
+    /// - Returns: An array of cities
+    ///
+    /// Calls on findLetterCheckPointAndSetRange(), which segments the json array of cities into sections based on first letter.
+    /// Then, calls getAutoCompleteCities() , which runs a binary search to find the first element that satisfies the autocomplete input within the range.
+    ///
+    /// (Ex: executes binary search within range of "L" for city names matching "Lon", then returns the amount specified by maxNumerOfResults)
+    static func searchForCities(cityName: String, maxNumberOfResults: Int) -> [City] {
         let capitalizedCityName = cityName.capitalized
         let cityNameCharacters = Array(capitalizedCityName)
 
@@ -22,38 +32,41 @@ class AutocompleteSearchManager {
                 
                 guard let jsonArray = jsonResult as? [Any] else {
                     print("Data of file could not be made casted into an array")
-                    return
+                    return []
                 }
                 
                 var range = Range(leftIndex: 0, rightIndex: jsonArray.count)
 
-                findLetterCheckPointAndSetRange(cityNameCharacters: cityNameCharacters, range: &range)
+                setRange(cityNameCharacters: cityNameCharacters, range: &range)
 //                print("range ---- leftIndex:", range.leftIndex)
 //                print("range ---- rightIndex:", range.rightIndex)
                 
                 let cities = getAutocompleteCities(jsonArray: jsonArray, range: range, input: cityNameCharacters, maxNumberOfResults: maxNumberOfResults)
-                cities.forEach { (city) in
-                    print(city.name)
-                }
+//                cities.forEach { (city) in
+//                    print(city.name)
+//                }
+                return cities
                 
             } catch {
                // handle error
                 print("caught error")
+                return []
             }
         }
+        return []
     }
     
+    /// Finds first alphanumeric city name within JSON file, and returns an array of cities that match.
+    ///
+    /// - Parameters:
+    ///     - jsonArray: Array of JSON data with city objects.
+    ///     - range: Object of type Range, with a left index and a right index, to be searched.
+    ///     - input: Input of the user for autocomplete search.
+    ///     - maxNumberOfResults: The maximum number or cities to return.
+    /// - Returns: An array of cities with the desired quantity.
     static func getAutocompleteCities(jsonArray: [Any], range: Range, input: [Character], maxNumberOfResults: Int) -> [City] {
-        // it is a modified binary search
         
         var cities = [City]()
-        // goal is to use binary search to search within the range for first appearance of city name matching what the user has typed
-        // ex: man == manhattan, manhasset, mandalorian, etc.
-        // we want to display at max 10 results.
-        
-        // maybe what we can do is do binary search until we find something that satisfies
-        // then move left until we find what doesn't. using what we have cached (the places we've already looked on the left)
-        // then, if we haven't hit our max of 10, we can also move rightwards too
         
         guard let firstMatchIndex = findFirstMatchIndex(jsonArray: jsonArray, range: range, input: input) else {
             return []
@@ -98,6 +111,21 @@ class AutocompleteSearchManager {
         return cities
     }
     
+    /// Uses modified binary search to find first index of a city name that matches the user's input.
+    ///
+    /// - Parameters:
+    ///     - jsonArray: An array of JSON city objects.
+    ///     - range: Object of type Range, with a left index and a right index, to be searched.
+    ///     - input: Input of the user for autocomplete search.
+    /// - Returns: An optional Int of the index of the first matching city.
+    ///
+    /// Uses binary search to find a city name that matches the user's input.
+    ///
+    /// Ex: Input: "Man" --- "Manhattan", "Manhasset", "Mandalorian", etc.
+    ///
+    /// Once an element is found that matches the requirements, the binary search will jump left, until there are only two indexes left, of which one is the starting index of the pattern.
+    ///
+    /// One optimization is that if the middle object chosen matches exactly with the user's input, (ex. "Man" and "Man"), then it will check the indexes before it to see if there are any other cities with the same name which would come first, then it would return that index.
     static func findFirstMatchIndex(jsonArray: [Any], range: Range, input: [Character]) -> Int? {
         if jsonArray.count == 0 {
             return nil
@@ -111,7 +139,6 @@ class AutocompleteSearchManager {
             let middleIndex = (rightIndex + leftIndex) / 2
             finalIndex = middleIndex
             guard let middleObject = jsonArray[middleIndex] as? [String: Any] else {
-                // i think return empty if the middle element doesn't exist, or isn't a dictionary?
                 return nil
             }
             guard let middlePointCityName = middleObject["name"] as? String else {
@@ -121,12 +148,12 @@ class AutocompleteSearchManager {
             let midPointCityCharacters = Array(middlePointCityName)
             
             if midPointCityCharacters.count >= input.count && Array(midPointCityCharacters[0...input.count - 1]) == input {
-                // i've found something that satisfies
+                // There is a match
                 
                 // Optimization: check if it is an exact match
                 if midPointCityCharacters.count == input.count {
                     print("there's an exact match at index: \(middleIndex). City: \(middlePointCityName)")
-                    // move left until it is not. (just to check)
+                    // Move left to see if leftward city names are exactly the same as well. Returns first same city name.
                     
                     var index = middleIndex
                     while index - 1 >= 0 {
@@ -144,9 +171,7 @@ class AutocompleteSearchManager {
                     }
                 }
                 
-                // middleIndex is where something satisfies
                 print("found a city that satisfies the autocorrect of \(String(input)). it is: \(middlePointCityName) at index: \(middleIndex), moving LEFT")
-                // now move left until we find the point where it stops satisfying
       
                 rightIndex = middleIndex - 1
             }
@@ -168,7 +193,7 @@ class AutocompleteSearchManager {
             return nil
         }
         
-        // the answer could be the final Index OR final index + 1
+        // the answer could be the final Index OR final index + 1, so we need to check that here.
         guard let cityObject = jsonArray[index] as? [String: Any] else {
             // i think return empty if the middle element doesn't exist, or isn't a dictionary?
             return nil
@@ -201,7 +226,13 @@ class AutocompleteSearchManager {
         }
     }
     
-    static func findLetterCheckPointAndSetRange(cityNameCharacters: [Character], range: inout Range) {
+    /// Sets range for city name to be searched upon. The JSON file of cities is segmented into sections each beginning with a letter. This method finds the corresponding section based on the first letter of the input.
+    ///
+    /// - Parameters:
+    ///     - cityNameCharacters: An array of characters for the input the user typed in to be searched for.
+    ///     - range: An inout property for the range to be searched on.
+    /// - Returns: Void
+    static func setRange(cityNameCharacters: [Character], range: inout Range) {
         switch cityNameCharacters[0] {
         case "A":
             range = CityListCheckPoints.letterA
