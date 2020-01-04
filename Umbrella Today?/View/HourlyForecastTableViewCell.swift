7267ba67fdf907fba8a49fd6488b353461552315
Brandon Fong <bfongbfong@gmail.com>
2020-01-03 22:43:26 -0500

    Organizes

--- Umbrella Today?/View/HourlyForecastTableViewCell.swift
+++ Umbrella Today?/View/HourlyForecastTableViewCell.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class HourlyForecastTableViewCell: UITableViewCell {
     
+    // MARK: - Outlets
     @IBOutlet weak var dayLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var weatherImageView: UIImageView!
     
+    // MARK: - Properties
     var isDaytime = false {
         didSet {
             if isDaytime {
@@ -37,18 +39,13 @@ class HourlyForecastTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
     
-    // TODOL - figure out why colors are reversed
-    
-    // need to pass in min temp
     func update(time: String, currentTemp: Int, description: String, isDaytime: Bool) {
         dayLabel.text = time
         weatherImageView.image = WeatherImages.getImage(weatherDescription: description, isDaytime: isDaytime)
